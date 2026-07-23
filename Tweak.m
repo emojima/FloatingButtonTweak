764c428e0a9bf90f0c46d28a5cebae67896f916b
emojima <75198706+emojima@users.noreply.github.com>
2026-07-23 19:29:51 +0800

    Update Tweak.m

--- Tweak.m
+++ Tweak.m
@@ -136,6 +136,14 @@
     if (self.logContainerView) {
         self.logContainerView.hidden = NO;
         self.isVisible = YES;
+        // 还原到默认位置
+        CGFloat screenWidth = [[UIScreen mainScreen] bounds].size.width;
+        CGFloat screenHeight = [[UIScreen mainScreen] bounds].size.height;
+        CGFloat windowWidth = screenWidth * 0.9;
+        CGFloat windowHeight = screenHeight * 0.55;
+        CGFloat windowX = (screenWidth - windowWidth) / 2;
+        CGFloat windowY = screenHeight * 0.12;
+        self.logContainerView.frame = CGRectMake(windowX, windowY, windowWidth, windowHeight);
         UIWindow *topWindow = [self topmostWindow];
         if (topWindow) {
             UIView *panelOverlay = [topWindow viewWithTag:99999];
@@ -228,7 +236,15 @@
     }
 
     self.logContainerView.hidden = NO;
-    self.isVisible = YES;
+        self.isVisible = YES;
+        // 还原到默认位置
+        CGFloat screenWidth = [[UIScreen mainScreen] bounds].size.width;
+        CGFloat screenHeight = [[UIScreen mainScreen] bounds].size.height;
+        CGFloat windowWidth = screenWidth * 0.9;
+        CGFloat windowHeight = screenHeight * 0.55;
+        CGFloat windowX = (screenWidth - windowWidth) / 2;
+        CGFloat windowY = screenHeight * 0.12;
+        self.logContainerView.frame = CGRectMake(windowX, windowY, windowWidth, windowHeight);
     // 显示日志窗口后自动关闭功能面板（仅在面板已打开时）
     UIWindow *kw2 = [self topmostWindow];
     if (kw2 && [kw2 viewWithTag:99997]) {
@@ -329,10 +345,6 @@
         CGRect newFrame = self.logContainerView.frame;
         newFrame.origin.x += deltaX;
         newFrame.origin.y += deltaY;
-        CGFloat screenWidth = [[UIScreen mainScreen] bounds].size.width;
-        CGFloat screenHeight = [[UIScreen mainScreen] bounds].size.height;
-        newFrame.origin.x = MAX(0, MIN(newFrame.origin.x, screenWidth - newFrame.size.width));
-        newFrame.origin.y = MAX(0, MIN(newFrame.origin.y, screenHeight - newFrame.size.height));
         self.logContainerView.frame = newFrame;
         self.lastTranslation = translation;
     }
